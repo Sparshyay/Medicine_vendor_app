673dc9bad1c27eb3061838f4010388b0b5186e27
Your Name <sicario.official28@gmail.com>
2025-05-20 02:46:48 +0530

    Update web deployment with fixed configuration for hosting compatibility

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2059573231"
+    serviceWorkerVersion: "697241287"
   }
 });
